f528262b0beace6ea623eb5e108e9ee0dc01a00d
Ajay David <antonio.g.david@gmail.com>
2018-05-19 18:17:33 -0400

    create SuccessAlert component, register, style, and display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WarningAlert } from '../warning_alert/warning_alert.component';
-
+import { SuccessAlert } from '../success_alert/success_alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WarningAlert
+    WarningAlert,
+    SuccessAlert
   ],
   imports: [
     BrowserModule
